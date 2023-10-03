58014ecfb56fca68346f0836af213ab33e20330b
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-03 15:58:39 -0500

    added the foreign keys to create the employee table

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -19,6 +19,11 @@ CREATE TABlE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INT NOT NULL
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id), 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
